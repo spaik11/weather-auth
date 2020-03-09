e3181f806f7513b4f0c67e323665e0a9a601b050
Sung Paik <sung.paik@codeimmersives.com>
2020-03-09 11:40:56 -0400

    fixed location

--- routes/weather/controllers/weatherController.js
+++ routes/weather/controllers/weatherController.js
@@ -8,10 +8,17 @@ module.exports = {
 
     getWeather: async(req, res, next) => {
         try {
-            const location = await places.filter((place) => place.location === 'NYC');
-            const response = await axios.get(`https://api.darksky.net/forecast/${process.env.API_KEY}/${location[0].lat},${location[0].long}?units=us`);
-            const currentWeather = response.data;
-            res.render('auth/weather', { currentWeather, places, location });
+            if (req.query.location) {
+                const location = await places.filter((place) => place.location === req.query.location);
+                const response = await axios.get(`https://api.darksky.net/forecast/${process.env.API_KEY}/${location[0].lat},${location[0].long}?units=us`);
+                const currentWeather = response.data;
+                res.render('auth/weather', { currentWeather, places, location });
+            } else {
+                const location = await places.filter((place) => place.location === 'NYC');
+                const response = await axios.get(`https://api.darksky.net/forecast/${process.env.API_KEY}/${location[0].lat},${location[0].long}?units=us`);
+                const currentWeather = response.data;
+                res.render('auth/weather', { currentWeather, places, location });
+            }
         } catch (error) {
             console.log('error!!!', error);
             res.redirect('/');
